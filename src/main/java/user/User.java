5054a2d6f625eb140cee91c2ba312fa88f2aaa74
Phu Dep Trai <phujason1992@gmail.com>
2025-09-10 16:00:05 +0700

    abc

---
+++ src/main/java/user/User.java
@@ -0,0 +1,25 @@
+package user;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
